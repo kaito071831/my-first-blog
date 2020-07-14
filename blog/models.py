32bf2c6cfddc72958cf5578773f1344dcebd5e20
Your name <you@example.com>
2020-07-14 14:30:59 +0900

    Add draft and comment function.

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,20 @@ class Post(models.Model):
     
     def _str_(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    apporved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+    
+    def apporved_comment(self):
+        return self.comment.filter(approved_comment=True)
